2aed23e364dd83eaf4793856a84d61166285115d
IsisG13 <isissouzaguimaraes@gmail.com>
2022-12-07 11:24:08 -0300

    criação de Header Simples

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
+import './index.css'
 
 const Header = () => {
-  return <div>Header</div>
+  return (
+    <header className="header">
+      <h1>Filmes e Séries</h1>
+
+      <nav>
+        <ul className="listNav">
+          <li className="item">Filmes</li>
+          <li className="item">Séries</li>
+          <li className="item">Contato</li>
+        </ul>
+      </nav>
+    </header>
+  )
 }
 
 export default Header
