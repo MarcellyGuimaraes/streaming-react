8bf132f17a9519d1e5567abc04b1f41cb559c77c
IsisG13 <isissouzaguimaraes@gmail.com>
2022-12-07 12:01:23 -0300

    criação de Home

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,7 +1,26 @@
 import React from 'react'
+import './index.css'
+import filmes from '../../assets/img/filmes.jpg'
+import series from '../../assets/img/series.jpg'
 
 const Home = () => {
-  return <div>Home</div>
+  return (
+    <div className="container">
+      <a href="../Series">
+        <div className="card">
+          <img src={series} alt="series card" />
+          <p>Séries Populares</p>
+        </div>
+      </a>
+
+      <a href="../Filmes">
+        <div className="card">
+          <img src={filmes} alt="filmes card" />
+          <p>Filmes Populares</p>
+        </div>
+      </a>
+    </div>
+  )
 }
 
 export default Home
