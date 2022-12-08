6b0056761e772767919496a83de6c3754b96f991
IsisG13 <isissouzaguimaraes@gmail.com>
2022-12-08 08:56:49 -0300

    arrumando detalhe no css e link de contato no header e footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 import './index.css'
 
 const Footer = () => {
-  return <div className="footer">Desenvolvido por Marcelly Guimarães ^^</div>
+  return (
+    <div className="footer">
+      Desenvolvido por{' '}
+      <a href="https://github.com/MarcellyGuimaraes"> Marcelly Guimarães</a> ^^
+    </div>
+  )
 }
 
 export default Footer
