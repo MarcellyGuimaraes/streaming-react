8a06929de70bf442a9fec7f6d12f3b1477841a38
MarcellyGuimaraes <souzaguimaraesmarcelly@gmail.com>
2023-01-25 17:02:17 -0300

    feat: colocando tailwind no header (faltam alterações)

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,26 +4,26 @@ import './index.css'
 
 const Header = () => {
   return (
-    <header className="header">
+    <header className="flex md:flex-grow flex-col bg-pink-500 items-center justify-between px-2 py-3">
       <Link to="/">
-        <h1 className="title">DEMO Streaming</h1>
+        <h1 className="text-white font-bold">DEMO Streaming</h1>
       </Link>
       <nav>
-        <ul className="listNav">
-          <li className="item">
-            <Link className="title" to="/filmes">
+        <ul className="flex list-none items-center justify-between">
+          <li className="px-1">
+            <Link className="text-white font-bold" to="/filmes">
               Filmes
             </Link>
           </li>
-          <li className="item">
-            <Link className="title" to="/series">
+          <li className="px-1">
+            <Link className="text-white font-bold" to="/series">
               Séries
             </Link>
           </li>
-          <li className="item">
+          <li className="px-1">
             <a
               target="_blank"
-              className="title"
+              className="text-white font-bold"
               href="https://www.linkedin.com/in/marcelly-souza-guimar%C3%A3es-728948246/"
               rel="noreferrer"
             >
