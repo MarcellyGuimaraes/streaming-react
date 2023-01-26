91c2887858293341aff918b58b953f3dab0ea486
MarcellyGuimaraes <souzaguimaraesmarcelly@gmail.com>
2023-01-26 17:30:21 -0300

    fix: arrumando o css da home com tailwind

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -6,20 +6,22 @@ import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
-    <div className="flex">
-      <h2>Veja aqui as séries e filmes mais populares de hoje!</h2>
+    <div className="flex flex-col items-center justify-center h-[85vh]">
+      <h2 className="text-xl font-bold">
+        Veja aqui as séries e filmes mais populares de hoje!
+      </h2>
       <div className="flex">
         <Link to="/series">
-          <div className="m-1">
+          <div className="m-4 pb-16 w-44 h-80">
             <img src={series} alt="series card" />
-            <span>Séries Populares</span>
+            <span className="m-4 text-center">Séries Populares</span>
           </div>
         </Link>
 
         <Link to="/filmes">
-          <div className="m-1">
+          <div className="m-4 pb-16 w-44 h-80">
             <img src={filmes} alt="filmes card" />
-            <span>Filmes Populares</span>
+            <span className="m-4 text-center">Filmes Populares</span>
           </div>
         </Link>
       </div>
